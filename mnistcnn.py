00a681a6966d1b544773c9bd460cfb68437216ed
Robbie West <100315505+Kerch0O@users.noreply.github.com>
2023-04-12 13:49:03 +0100

    hey

--- mnistcnn.py
+++ mnistcnn.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-def readMnist(num_images):
+def readMnist(num_images, traintest):
     
     #Images
-    f = gzip.open('./mnistdata/train-images-idx3-ubyte.gz','r')
+    f = gzip.open('./mnistdata/{}-images-idx3-ubyte.gz'.format(traintest),'r')
 
     image_size = 28
 
@@ -15,17 +15,16 @@ def readMnist(num_images):
     data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
     data = data.reshape(num_images, image_size, image_size, 1)
 
-    images = [np.asarray(data[x]).squeeze() for x in range(num_images)]
+    images = [np.asarray(data[x] / 255.0).squeeze() for x in range(num_images)]
 
     #Labels
-    f = gzip.open('./mnistdata/train-labels-idx1-ubyte.gz','r')
+    f = gzip.open('./mnistdata/{}-labels-idx1-ubyte.gz'.format(traintest),'r')
     f.read(8)
     buf = f.read(num_images)
     labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
 
     return images, labels
 
-images, labels = readMnist(60000)
 
 class ConvLayer:
     def __init__(self, depth, input_depth, input_shape, kernel_size):
@@ -38,23 +37,138 @@ class ConvLayer:
         self.biases = np.random.randn(self.output_shape)
 
     def forwards_prop(self, input):
-        self.input = input
-        self.output = self.biases
+        self.input = input.copy()
+        self.output = self.biases.copy()
 
         for i in range(self.input_depth):
             for j in range(self.depth):
-                self.output[i] += signal.convolve2d(input[j], self.kernels[i, j], "valid")
+                self.output[i] += signal.correlate2d(input[j], self.kernels[i, j], "valid")
 
         return self.output
     
 
     def backwards_prop(self, output_gradients, learning_rate):
 
-        input_gradients = self.input
+        input_gradients = self.input.copy()
+      
         for i in range(self.input_depth):
             for j in range(self.depth):
                 self.kernel[i, j] -= learning_rate * signal.correlate2d(self.input[j], output_gradients[i], "valid")
                 input_gradients[i] = signal.convolve2d(output_gradients[i], self.kernel[i, j], "full")
+      
         self.biases[i] -= learning_rate * output_gradients
 
         return -learning_rate * signal.convolve2d()
+
+class MaxPooling:
+    def __init__(self, shape, stride):
+        self.shape = shape
+        self.stride = stride 
+
+    def forward(self, input, input_shape):
+        output_shape = (input_shape[0], int(input_shape[1] / self.shape[0]), int(input_shape[2] / self.shape[1]))
+        
+        outputs = np.zeros(output_shape)
+
+        for m in range(input_shape[0]): #For each matrix
+            for i in range(0, input_shape[1], self.shape[0]): #Height of the matrix
+                for j in range(0, input_shape[2], self.shape[1]): #Width of the matrix
+                    outputs[m, int(i / self.shape[0]), int(j / self.shape[1])] = max(
+                        input[m, i, j], input[m, i + 1, j], input[m, i, j + 1], input[m, i + 1, j + 1]
+                    )
+
+        return outputs
+    
+    def backward(output_gradients):
+        pass
+
+class DenseLayer:
+    def __init__(self, input_shape, output_shape, activation_func):
+
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+
+        self.activations = np.random.randn(output_shape)
+        self.biases = np.random.randn(output_shape)
+        self.weights_shape = (output_shape, input_shape)
+        self.weights = np.random.randn(self.weights_shape)
+        self.activation_func = activation_func
+
+        self.sums = np.zeros(output_shape)
+    
+
+    def forward(self, input):
+        sums = np.matmul(input, self.weights) + self.biases
+        self.activations = self.activation_func(sums, True)
+        return self.activations
+    
+    def backward(self, output_gradients, learning_rate):
+        
+        next_output_gradients = np.zeros(output_gradients.shape)
+
+        
+            
+                
+    
+def ReLU(inp, no_der):
+    output = np.zeros(inp.shape)
+    if no_der:
+        #Standard function not the derivative
+        output = np.array([(x > 0) * x for x in inp])
+    else:
+        #Derivative
+        output = np.array([x > 0  for x in inp])
+
+    return 0
+
+def small_softmax(inp, sums):
+    
+    output = np.exp(inp) / sums
+    return output
+
+def softmax(inp, no_der):
+    output = np.zeros(inp.shape)
+    exps = np.exp(inp)
+    sums = np.sum(exps)
+
+    if no_der:
+        #Standard function not the derivative
+        output = exps / sums
+    else:
+        #Derivative
+        for i in range(len(inp)):
+            for j in range(len(inp)):
+                if i == j:
+                    #DjSi = Sj(1 - Si)
+                    #Although i = j therefore Sj = Si
+                    output[i] = exps[j] / sums * (1.0 - exps[i] / sums)
+                else:
+                    #DjSi = -SiSj
+                    output[i] = -1.0 * ((exps[i] / sums) * (exps[j] / sums))
+    
+    return output
+
+'''
+Try network structure of:
+Input layer 28x28 grayscale 0 - 1
+Conv layer
+Max pooling 2x2 stride 2
+Conv layer
+Conv layer
+flatten layer
+dense layer
+dense layer
+output 0 - 9
+'''
+
+#images, labels = readMnist(60000, "train")
+
+''' Testing for max pooling
+test_input = np.array([[[5, 9, 8, 7],
+                       [3, 1, 5, 7],
+                       [9, 0, 8, 7],
+                       [3, 1, 2, 1]]])
+print(test_input)
+layer = MaxPooling((2, 2), 2)
+print(layer.forward(test_input, (1, 4, 4)))
+'''
